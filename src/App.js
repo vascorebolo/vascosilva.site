eb2882ccad3b73cf1632ad539d6650b815ebec78
Vasco Silva <vasco.s.rebolo@gmail.com>
2018-10-17 21:03:59 +0100

    Extract routes section to RenderZone component

--- src/App.js
+++ src/App.js
@@ -1,24 +1,10 @@
 import React, { Component } from 'react'
-import { Route } from 'react-router-dom'
 import MainNav from './components/MainNav'
+import RenderZone from './components/RenderZone'
 import routes from './menuconfig'
 import './App.css'
 
 class App extends Component {
-  // Will eventually put this in proper component for nav
-  renderRoutes() {
-      return routes.map((item, index) => {
-        let dynamicComponent = require('./components/' + item.component + '/' + item.component).default
-
-        return <Route
-                  key={`${index}-route`}
-                  path={item.path}
-                  exact={item.exact}
-                  component={dynamicComponent}
-                />
-      })
-  }
-
   render() {
     return (
       <div className="App">
@@ -27,9 +13,7 @@ class App extends Component {
           <nav>
             <MainNav routes={routes} />
           </nav>
-          <div>
-              { this.renderRoutes() }
-          </div>
+          <RenderZone routes={routes} />
         </header>
       </div>
     );
---
+++ src/components/RenderZone/RenderZone.js
@@ -0,0 +1,32 @@
+import React from 'react'
+import PropTypes from 'prop-types'
+import { Route } from 'react-router-dom'
+
+const RenderZone = ({ routes }) => {
+  function renderRoutes() {
+    return routes.map((item, index) => {
+      let dynamicComponent = require('../' + item.component + '/' + item.component).default
+
+      return <Route
+                key={`${index}-route`}
+                path={item.path}
+                exact={item.exact}
+                component={dynamicComponent}
+              />
+    })
+  }
+
+  return (
+    <div>
+      { renderRoutes() }
+    </div>
+  )
+}
+
+RenderZone.propTypes = {
+  routes: PropTypes.array
+}
+
+RenderZone.displayName = 'RenderZone'
+
+export default RenderZone
