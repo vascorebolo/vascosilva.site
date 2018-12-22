8e8fb10da7ac635a7eeb86c0799799ed4ec906f9
Vasco Silva <vasco.s.rebolo@gmail.com>
2018-12-22 21:29:52 +0000

    Add draggable and horizontal scroll to gallery

--- src/components/Portfolio/Portfolio.js
+++ src/components/Portfolio/Portfolio.js
@@ -3,12 +3,16 @@ import Loading from '../Loading'
 import './Portfolio.css'
 import ImageLoader from 'react-loading-image'
 import GalleryPhotoLoading from '../GalleryPhotoLoading'
+import HorizontalScroll from 'react-scroll-horizontal'
+import Media from 'react-media'
+import Draggable from 'react-draggable'
 
 class Portfolio extends Component {
   state = {
     galleries: [],
     gallery: null,
     isLoadingNames: true,
+    deltaX: 0,
   }
 
   componentWillMount() {
@@ -37,36 +41,97 @@ class Portfolio extends Component {
     )
   }
 
+  renderGalleryInfo(gallery) {
+    return (
+      <div className="gallery-info" key={`${gallery.title}-info`}>
+        <h3>{gallery.title}</h3>
+        {gallery.description}
+      </div>
+    )
+  }
+
+  renderGalleryImages(gallery) {
+    return (
+      <div className="gallery">
+        { this.renderGalleryInfo(gallery) }
+        {
+          gallery.photos.map((photo, index) => {
+            return (
+              <ImageLoader
+                key={`imageloader-${gallery.title}-${index}`}
+                src={`https://vascosilva.site${photo.path}`}
+                loading={() => <GalleryPhotoLoading />}
+                image={props => <img
+                  src={`https://vascosilva.site${photo.path}`}
+                  key={`photo-${gallery.title}-${index}`}
+                  alt={`${gallery.title}-${index}`}
+                /> }
+                error={() => <div>Error</div>}
+              />
+            )
+          })
+        }
+      </div>
+    )
+  }
+
+  renderImages(gallery) {
+    let items = []
+
+    items.push(this.renderGalleryInfo(gallery))
+
+    gallery.photos.forEach((photo, index) => {
+      items.push(
+        <ImageLoader
+          key={`imageloader-${gallery.title}-${index}`}
+          src={`https://vascosilva.site${photo.path}`}
+          loading={() => <GalleryPhotoLoading />}
+          image={props => <img
+            src={`https://vascosilva.site${photo.path}`}
+            key={`photo-${gallery.title}-${index}`}
+            alt={`${gallery.title}-${index}`}
+          /> }
+          error={() => <div>Error</div>}
+        />
+      )
+    })
+
+    return items
+  }
+
+  handleDrag = (e,data) => {
+    this.setState({ deltaX: data.deltaX * 2 })
+  }
+
   renderGallery() {
     let gallery = this.state.gallery
 
     if (gallery != null) {
       return (
         <>
-          <div className="gallery">
-            <div className="gallery-info">
-              <h3>{gallery.title}</h3>
-              {gallery.description}
-            </div>
+        <Media query="(min-width: 880px)">
+          {matches =>
+            matches ? (
+              <Draggable
+                axis="x"
+                onDrag={this.handleDrag}
+              >
+                <div>
+                  <HorizontalScroll
+                    pageLock      = { true }
+                    reverseScroll = { true }
+                    animValues={this.state.deltaX}
+                  >
+                    { this.renderImages(gallery) }
+                  </HorizontalScroll>
+                </div>
+              </Draggable>
+            ) : (
+              this.renderGalleryImages(gallery)
+            )
+          }
+        </Media>
 
-            {
-              gallery.photos.map((photo, index) => {
-                return (
-                  <ImageLoader
-                    key={`imageloader-${gallery.title}-${index}`}
-                    src={`https://vascosilva.site${photo.path}`}
-                    loading={() => <GalleryPhotoLoading />}
-                    image={props => <img
-                      src={`https://vascosilva.site${photo.path}`}
-                      key={`photo-${gallery.title}-${index}`}
-                      alt={`${gallery.title}-${index}`}
-                    /> }
-                    error={() => <div>Error</div>}
-                  />
-                )
-              })
-            }
-          </div>
         </>
       )
     } else {
