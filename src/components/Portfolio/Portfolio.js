169b845f684b4e2ca3c677e074b181b66f6560bb
Vasco Silva <vasco.s.rebolo@gmail.com>
2019-01-03 21:36:19 +0000

    wip

--- src/components/Portfolio/Portfolio.js
+++ src/components/Portfolio/Portfolio.js
@@ -38,7 +38,7 @@ class Portfolio extends Component {
       // >
       //   {gallery.title}
       // </button>
-      <Link to={`${this.props.match.path}/${gallery._id}`} key={`gallery-${gallery._id}`} data-gallery={index} onClick={this.handleClickGallerySelect.bind(this)}>{gallery.title}</Link>
+      <Link to={`${this.props.match.path}/${gallery._id}`} key={`gallery-${gallery._id}`} data-gallery={index}>{gallery.title}</Link>
     )
   }
 
@@ -127,7 +127,7 @@ class Portfolio extends Component {
         </div>
         <div className="gallery-container h100">
           <h1>test</h1>
-          <Route path={`${match.path}/:topicId`} component={this.myGallery.bind(this)} />
+          <Route path={`${match.path}/:topicId`} component={GalleryRoute} />
           <Route
             exact
             path={match.path}
@@ -142,62 +142,8 @@ class Portfolio extends Component {
 
 export default Portfolio
 
-const GalleryRoute = ({gallery, isLoadingNames}) => {
-
-  const renderImages = (gallery) => {
-    let items = []
-
-
-    gallery.photos.forEach((photo, index) => {
-      items.push(
-        <ImageLoader
-          key={`imageloader-${gallery.title}-${index}`}
-          src={`https://vascosilva.site${photo.path}`}
-          loading={() => <GalleryPhotoLoading />}
-          image={props => <img
-            src={`https://vascosilva.site${photo.path}`}
-            key={`photo-${gallery.title}-${index}`}
-            alt={`${gallery.title}-${index}`}
-          /> }
-          error={() => <div>Error</div>}
-        />
-      )
-    })
-
-    return items
-  }
-
-  const renderGallery = () => {
-    console.table(gallery);
-
-    if (gallery != null) {
-      return (
-        <>
-
-
-              {  renderImages(gallery) }
-
-        </>
-      )
-    } else {
-      return isLoadingNames
-        ? null
-        : <>
-            <br />
-            <p>A space to share my photos. A space that I can practice some kind of curation to my photographic endeavors.</p>
-            <p>
-              I would be lying if I told you I only make photos to myself. I do it for my own pleasure, putting my personal
-              tastes and feelings in everything I point my camera to, but the ultimate goal is always to share my vision, my
-              way of looking into the world.
-            </p>
-            <p>
-              I consider myself lucky if I can "touch" someone, somehow...
-            </p>
-          </>
-    }
-  }
-
+const GalleryRoute = ({match}) => {
   return (
-    renderGallery()
+    <p>{match.params.topicId}</p>
   )
 }
